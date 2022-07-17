db239a1e65f5d789f91378c1b730a42f7ea5a750
Ahmed Saber <ahmed0saber33@gmail.com>
2022-07-17 17:45:23 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -39,6 +39,9 @@ autowatsForm.innerHTML = `<div class="section">
 	<div class="sent-successfully">
 		<span>Sent Successfully</span>
 	</div>
+	<div onclick="toggleAutowatsEmbededForm()" class="close">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg>
+	</div>
 </div>`
 document.body.appendChild(autowatsForm)
 
@@ -139,13 +142,13 @@ function autowatsSubmitForm(e){
 		setTimeout(()=>{
 			document.querySelector(".autowats-embeded-form .form").style.opacity = "0"
 			document.querySelector(".autowats-embeded-form .sent-successfully").style.display = "flex"
-			setTimeout(()=>{
-				toggleAutowatsEmbededForm()
-				setTimeout(()=>{
-					document.querySelector(".autowats-embeded-form .form").style.opacity = "1"
-					document.querySelector(".autowats-embeded-form .sent-successfully").style.display = "none"
-				}, 400)
-			}, 1600)
+			// setTimeout(()=>{
+			// 	toggleAutowatsEmbededForm()
+			// 	setTimeout(()=>{
+			// 		document.querySelector(".autowats-embeded-form .form").style.opacity = "1"
+			// 		document.querySelector(".autowats-embeded-form .sent-successfully").style.display = "none"
+			// 	}, 400)
+			// }, 1600)
 		}, 800)
 	}else{
 		console.log("Not valid")
